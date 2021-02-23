f58e213ac75b28d54c4e9eb2d14d72cde149ec3e
andreistoc <andreistoc@gmail.com>
2021-02-23 22:00:58 +0200

    Fetching and saving ToDo Items

--- BlckbridsToDo/TasksView.swift
+++ BlckbridsToDo/TasksView.swift
@@ -12,13 +12,19 @@ struct TasksView: View {
     var rowHeight: CGFloat = 50
     var sampleTasks = ["Task One", "Task Two", "Task Three"]
     
+    
+    @Environment(\.managedObjectContext) var viewContext
+    @FetchRequest(entity: ToDoItem.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ToDoItem.createdAt, ascending: false)], predicate: NSPredicate(format: "taskDone = %d", false), animation: .default)
+    
+    var fetchedItems: FetchedResults<ToDoItem>
+    
     @State var newTaskTitle = ""
     
     var body: some View {
         List {
-            ForEach(sampleTasks, id: \.self) { item in
+            ForEach(fetchedItems, id: \.self) { item in
                 HStack {
-                    Text(item)
+                    Text(item.taskTitle ?? "Empty")
                     Spacer()
                     Image(systemName: "circle")
                         .imageScale(.large)
@@ -28,17 +34,41 @@ struct TasksView: View {
             
             HStack{
                 TextField("Add task...", text: $newTaskTitle, onCommit: {print("New task title entered.")})
-                Image(systemName: "plus").imageScale(.large)
+                Button(action: {
+                    saveTask()
+                }, label: {
+                    Image(systemName: "plus").imageScale(.large)
+                })
+                
             }.frame(height: rowHeight)
             
             Text("Tasks done").frame(height: rowHeight)
             
         }.listStyle(GroupedListStyle())
     }
+    
+    
+    func saveTask () {
+        guard self.newTaskTitle != "" else { return }
+        
+        let newToDoItem = ToDoItem(context: viewContext)
+        
+        newToDoItem.taskTitle = newTaskTitle
+        newToDoItem.createdAt = Date()
+        newToDoItem.taskDone = false
+        
+        do {
+            try viewContext.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        newTaskTitle = ""
+    }
 }
 
 struct TasksView_Previews: PreviewProvider {
     static var previews: some View {
-        TasksView()
+        TasksView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
